0363bb4f0333b360273ea69c9c73e09c4d877191
Jacob Rousseau <J.B.I.Rousseau@amsterdamumc.nl>
2025-12-01 14:08:05 +0000

    Ongoing work on the .gitignore generation with the Python script

--- .amsterdamumc-scripts/generate_gitignore.py
+++ .amsterdamumc-scripts/generate_gitignore.py
@@ -1,8 +1,7 @@
 import requests
 
 # URL to your central forbidden extension list
-EXT_URL = "https://raw.githubusercontent.com/bavadeve/org-security-workflows/main/forbidden-extensions.txt"
-
+EXT_URL = "https://raw.githubusercontent.com/AmsterdamUMC/amsterdamumc-windows-git-template/refs/heads/main/.amsterdamumc-scripts/forbidden-extensions.txt"
 
 def fetch_forbidden_extensions():
     response = requests.get(EXT_URL)
@@ -12,19 +11,28 @@ def fetch_forbidden_extensions():
 
 def generate_gitignore(extensions):
     lines = [
-        
-        "",
-        "# The 'data' directory is where all data should be placed which must not be included in",
-        "# Git and GitHub.",
-        "# For example output of runs, secrets (API-keys, passwords, private-keys)",
-        "\n",
-        "data/**",
-        "",
-        "# Auto-generated from forbidden-extensions.txt",
-        "# Do not edit this section manually. Edit the central file instead.",
         "#",
         "# AmsterdamUMC .gitignore file",
         "#",
+        "#",
+        "# Auto-generated from forbidden-extensions.txt",
+        "#",
+        "",
+        "",
+        "# The 'data' directory is where all data should be placed which must",
+        "# ** NOT ** be included in Git and GitHub.",
+        "# These include for example output of runs, secrets ( API-keys, ",
+        "# passwords, private-keys ) and all patient data.",
+        "",
+        "data/**",
+        "",
+        "# Ignore everthing in the .amsterdamumc-scripts directory. This line",
+        "# was uncommented by the installation script which researchers",
+        "# ran to setup their research software environment.",
+        "",
+        "# .amsterdamumc-scripts/**",
+        ".amsterdamumc-scripts/.gitignore",
+        ""
     ]
     for ext in extensions:
         ext = ext.strip()
